25afd35ac26623a5167427e06d3d5788e268270e
Lavanyamurugan2001 <114234594+Lavanyamurugan2001@users.noreply.github.com>
2022-10-22 23:48:12 +0530

    Create nortb.v

---
+++ nortb.v
@@ -0,0 +1,15 @@
+module nortb;
+reg a, b; 
+wire y; 
+norgatee norgate(.a(a), .b(b),.y(y));
+initial begin
+ a=1'b0;b= 1'b0;
+ #10 a =1'b0;b= 1'b1;
+ #10 a=1'b1;b= 1'b0;
+ #10 a=1'b1;b= 1'b1;
+ #10
+$finish;
+ end 
+always @(y)
+ $display( "time =%0t \tINPUT VALUES: \t a=%b b =%b \t output value y=%b",$time,a,b,y);
+endmodule
